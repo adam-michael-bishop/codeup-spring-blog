f7bbce7093732d0daf0661695ab11f39245419fb
Adam Bishop <adam.michael.bishop@gmail.com>
2023-03-27 11:50:24 -0500

    Refactored package model to models. Added annotations for Jpa

---
+++ src/main/java/com/codeup/codeupspringblog/models/Post.java
@@ -0,0 +1,23 @@
+package com.codeup.codeupspringblog.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 1024)
+    private String title;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String body;
+}
