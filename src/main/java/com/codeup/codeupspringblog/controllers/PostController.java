81b171ead80fc7f1fff01be34b171fc71b0b8684
Adam Bishop <adam.michael.bishop@gmail.com>
2023-03-27 11:51:57 -0500

    Refactored posts controller to pull post data from database in allPostsView and postView.

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,6 +1,8 @@
 package com.codeup.codeupspringblog.controllers;
 
-import com.codeup.codeupspringblog.model.Post;
+import com.codeup.codeupspringblog.models.Post;
+import com.codeup.codeupspringblog.repositories.PostRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,25 +10,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@AllArgsConstructor
 @Controller
 public class PostController {
+    private final PostRepository postRepo;
 
     @GetMapping("/posts")
     public String allPostsView(Model model) {
-        List<Post> posts = new ArrayList<>();
-        posts.add(new Post("My First Post", "This is exciting, blogging much fun."));
-        posts.add(new Post("My Last Post", "Screw this, y'all are wild."));
+        List<Post> posts = postRepo.findAll();
+//        posts.add(new Post("My First Post", "This is exciting, blogging much fun."));
+//        posts.add(new Post("My Last Post", "Screw this, y'all are wild."));
         model.addAttribute("posts", posts);
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    public String postView(@PathVariable int id, Model model) {
-        Post post = new Post("Placeholder Post", "This post is a placeholder that will eventually show the post with the id: " + id);
-        model.addAttribute("post", post);
+    public String postView(@PathVariable Long id, Model model) {
+        Optional<Post> post = postRepo.findById(id);
+        //        Post post = new Post("Placeholder Post", "This post is a placeholder that will eventually show the post with the id: " + id);
+        post.ifPresent(value -> model.addAttribute("post", value));
         return "posts/show";
     }
 
