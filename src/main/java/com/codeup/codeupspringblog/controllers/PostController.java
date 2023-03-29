4e6c75a4474ba8b93a8a65360f0758d5ca85a8b9
Adam Bishop <adam.michael.bishop@gmail.com>
2023-03-29 14:55:40 -0500

    Refactored postView, removed redundant dao method for finding user of a post. Refactored savePost method to a void method. Edit and create posts are done using Bootstrap modals rather than redirecting to another page

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,16 +1,15 @@
 package com.codeup.codeupspringblog.controllers;
 
 import com.codeup.codeupspringblog.models.Post;
-import com.codeup.codeupspringblog.models.User;
 import com.codeup.codeupspringblog.repositories.PostRepository;
 import com.codeup.codeupspringblog.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @AllArgsConstructor
 @Controller
@@ -22,19 +21,17 @@ public class PostController {
     @GetMapping
     public String allPostsView(Model model) {
         List<Post> posts = postRepo.findAll();
+        Post post = new Post();
         model.addAttribute("posts", posts);
+        model.addAttribute("post", post);
         return "posts/index";
     }
 
     @GetMapping("/{id}")
     public String postView(@PathVariable Long id, Model model) {
-        Optional<Post> post = postRepo.findById(id);
+        model.addAttribute("editing", true);
         try {
-            post.ifPresent(newPost -> {
-                model.addAttribute("post", newPost);
-                Optional<User> user = userRepo.findById(newPost.getUser().getId());
-                user.ifPresent(newUser -> model.addAttribute("user", newUser));
-            });
+            postRepo.findById(id).ifPresent(post -> model.addAttribute("post", post));
         } catch (NullPointerException e) {
             System.out.println(e);
             //replace with error page
@@ -51,18 +48,21 @@ public class PostController {
     }
 
     @PostMapping("/save")
-    public String savePost(@ModelAttribute Post post) {
+    @ResponseStatus(HttpStatus.OK)
+    public void savePost(@ModelAttribute Post post) {
         try {
             userRepo.findById(1L).ifPresent(post::setUser);
         } catch (NullPointerException e) {
             System.out.println(e);
             //replace with error page
-            return "redirect:/posts";
+//            return "redirect:/posts";
         }
         postRepo.save(post);
-        return "redirect:/posts";
+//        return "redirect:/posts";
     }
 
+    //Not using postsEditView anymore, it can still be accessed by url
+    @Deprecated
     @GetMapping("/{id}/edit")
     public String postsEditView(Model model, @PathVariable Long id) {
         try {
@@ -72,6 +72,6 @@ public class PostController {
             //replace with error page
             return "redirect:/posts";
         }
-        return "posts/edit";
+        return "posts/old-edit";
     }
 }
