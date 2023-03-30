56057ca69422bc82c16efc21e3fae030033a9f71
Adam Bishop <adam.michael.bishop@gmail.com>
2023-03-30 10:32:43 -0500

    Added better user feedback when form submit fails

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,27 +4,33 @@ $(function () {
 		$("#form-submit").text("Save Changes").removeClass().addClass(["btn", "btn-primary"]).prop("disabled", false);
 	}
 
-	function submitForm(data) {
+	function submitForm(data, url) {
 		return $.ajax({
-			url: "/posts/save",
+			url: url,
 			type: "POST",
 			data: data
 		})
 	}
 
-	function showSavingButton() {
+	function showSavingButtonSpinner() {
 		$("#form-submit").html(`<span class="spinner-border spinner-border-sm"></span> Saving...`).prop("disabled", true);
+	}
+
+	function setServerErrorText() {
+		$("#submission-message").text("Epic failure has occurred. A team of genetically enhanced hamsters is busy working on the problem now.")
+		$("#form-submit").text("Resubmit").addClass("btn-warning").removeClass("btn-primary").prop("disabled", false)
 	}
 
 	$("#edit-post-form").on("submit", function (e) {
 		e.preventDefault();
-		showSavingButton();
-		submitForm($(this).serialize())
+		showSavingButtonSpinner();
+		submitForm($(this).serialize(), "/posts/save")
 			.done(function () {
-				$("#form-submit").text("Changes Saved").addClass("btn-success").removeClass("btn-primary")
+				$("#form-submit").text("Changes Saved").addClass("btn-success").removeClass("btn-primary");
+				$("#edit-post-modal").on("hide.bs.modal", location.reload.bind(location));
 			})
 			.fail(function () {
-				$("#form-submit").text("Epic Failure").addClass("btn-danger").removeClass("btn-primary")
+				setServerErrorText();
 			});
 	}).find(":input").each(function () {
 		$(this).on("input propertychange paste", resetSubmitButton);
@@ -32,14 +38,14 @@ $(function () {
 
 	$("#create-post-form").on("submit", function (e) {
 		e.preventDefault();
-		showSavingButton();
-		submitForm($(this).serialize())
+		showSavingButtonSpinner();
+		submitForm($(this).serialize(), "/posts/create")
 			.done(function () {
 				resetSubmitButton();
 				location.reload();
 			})
 			.fail(function () {
-				$("#form-submit").text("Epic Failure").addClass("btn-danger").removeClass("btn-primary")
+				setServerErrorText();
 			});
-	})
+	});
 });
